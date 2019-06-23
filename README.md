87ca3f7908f0581456e4db4b286d5d1e7cc7c51e
Radon Rosborough <radon.neon@gmail.com>
2019-06-22 19:36:57 -0700

    Add Emacs Lisp project template

---
+++ README.md
@@ -0,0 +1,6 @@
+# Mercury
+
+Mercury is an [Emacs](https://www.gnu.org/software/emacs/) interface
+to [Facebook Messenger](https://www.messenger.com/).
+
+It is currently under construction.
---
+++ mercury.el
@@ -0,0 +1,37 @@
+;;; mercury.el --- Emacs interface to FB Messenger. -*- lexical-binding: t -*-
+
+;; Copyright (C) 2019 Radon Rosborough
+
+;; Author: Radon Rosborough <radon.neon@gmail.com>
+;; Created: 30 Apr 2019
+;; Homepage: https://github.com/raxod502/mercury
+;; Keywords: applications
+;; Package-Requires: ((emacs "26"))
+;; Version: 0
+
+;;; Commentary:
+
+;; TODO
+
+;; Please see <https://github.com/raxod502/mercury> for more
+;; information.
+
+;;; Code:
+
+;; To see the outline of this file, run M-x outline-minor-mode and
+;; then press C-c @ C-t. To also show the top-level functions and
+;; variable declarations in each section, run M-x occur with the
+;; following query: ^;;;;* \|^(
+
+;; TODO
+
+;;;; Closing remarks
+
+(provide 'mercury)
+
+;; Local Variables:
+;; indent-tabs-mode: nil
+;; outline-regexp: ";;;;* "
+;; End:
+
+;;; mercury.el ends here
